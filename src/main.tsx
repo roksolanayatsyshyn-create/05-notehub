0e6e1122a9447367f4846469bf39b84422fbd45a
Roksolana Fedyna <roksolanayatsyshyn@gmail.com>
2025-12-25 22:22:06 +0200

    modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import 'modern-normalize';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
